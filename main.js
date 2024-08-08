7b3241aa3a7e525ad29212db23e90a7357c9c902
sprd39 <76632782+sprd39@users.noreply.github.com>
2024-08-08 15:33:10 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -16,8 +16,6 @@ Webcam.set({
     jpeg_quality:90
 });
 
-
-
 function speak(){
 
     
@@ -29,4 +27,29 @@ function speak(){
     synth.speak(utterThis);
 
 
+}
+
+setTimeout(function()
+{
+    img_id = "selfie1";
+    take_snapshot();
+    speak_data = "Taking your next Selfie in 10 seconds";
+    var utterThis = new SpeechSynthesisUtterance
+}, 5000);
+
+function take_snapshot()
+{
+    console.log(img.id);
+    
+    Webcam.snap(function(data_uri) {
+        if(img_id=="selfie1"){
+            document.getElementById("result1").innerHTML = '<img id="selfie1" src="'+data_uri+'"/>'
+        }
+        if(img_id=="selfie2"){
+            document.getElementById("result1").innerHTML = '<img id="selfie2" src="'+data_uri+'"/>'
+        }
+        if(img_id=="selfie3"){
+            document.getElementById("result1").innerHTML = '<img id="selfie3" src="'+data_uri+'"/>'
+        }
+    });
 }
